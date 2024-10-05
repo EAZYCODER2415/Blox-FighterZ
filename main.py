f5c31a485b4aca34b828df175d77ce495202f9ec
Pongsavaruth (Thames) Vorajayudhbong <thames2415@gmail.com>
2024-10-05 16:23:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
 import random
 import os
 import time
-
-def choose_character(option):
-    if option == "a" or option == 1:
-        character = "Noob"
-    elif option == "b" or option == 2:
-        character = "Bacon"
-    elif option == "c" or option == 3:
-        character = "Pizza Man"
-    elif option == "d" or option == 4:
-        character = "Murderer"
-    elif option == "e" or option == 5:
-        character = "Criminal"
-    elif option == "f" or option == 6:
-        character = "Cop"
-    elif option == "g" or option == 7:
-        character = "Piggy"
-    elif option == "h" or option == 8:
-        character = "Boxer"
-    elif option == "i" or option == 9:
-        character = "Ninja"
-    elif option == "j" or option == 10:
-        character = "Baller"
-    return character
+from CC import choose_character
 
 def print_options(option, character, hp_regen_count, energy_regen_count, attack_item_count):
     if int(option) == 1:
