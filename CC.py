0aea4e9181a9c69317f2ad5c748dce5b48d9680b
Pongsavaruth (Thames) Vorajayudhbong <thames2415@gmail.com>
2024-10-05 16:22:42 +0700

    Create CC.py

---
+++ CC.py
@@ -0,0 +1,22 @@
+def choose_character(option):
+    if option == "a" or option == 1:
+        character = "Noob"
+    elif option == "b" or option == 2:
+        character = "Bacon"
+    elif option == "c" or option == 3:
+        character = "Pizza Man"
+    elif option == "d" or option == 4:
+        character = "Murderer"
+    elif option == "e" or option == 5:
+        character = "Criminal"
+    elif option == "f" or option == 6:
+        character = "Cop"
+    elif option == "g" or option == 7:
+        character = "Piggy"
+    elif option == "h" or option == 8:
+        character = "Boxer"
+    elif option == "i" or option == 9:
+        character = "Ninja"
+    elif option == "j" or option == 10:
+        character = "Baller"
+    return character
